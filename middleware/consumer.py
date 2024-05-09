39727577da19d46beb47624e2ef80ac23c18b7a4
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-05-09 15:27:53 +0300

    Add files via upload

---
+++ middleware/consumer.py
@@ -0,0 +1,26 @@
+from kafka import KafkaConsumer
+
+import json as js
+
+def main():
+
+    #try :
+    consumer = KafkaConsumer('vehicle_positions')
+    while 1 : # runs 4ever
+        batches = {}
+        for i in consumer :# for every message in q
+            json_data = js.loads(i.value.decode('utf-8'))
+            # consumer must make batches
+            # 1 Extract time
+            current_time = json_data['time']
+            # 2 Time does not exist
+            if not (str(current_time) in batches.keys()):
+                batches[current_time] = {'time' : current_time , 'link' : json_data['link'] , 'vcount' : 0 , 'vspeed' : 0}
+            # 3 Add data in the correct batch
+            batches[current_time]['vcount'] += 1
+            batches[current_time]['vspeed'] = (batches[current_time]['vspeed'] + json_data['v'])/2
+        for i in batches :
+            print(str(batches[i]))
+
+if __name__=='__main__':
+    main()
---
+++ producer.py
@@ -0,0 +1,49 @@
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+import json as js
+import pandas as pd
+import numpy as np
+
+import datetime as dt
+
+def getVehicles():
+    df = pd.read_csv('vehicle_dataset.csv')# read csv file
+    df.drop(columns=['Unnamed: 0'], inplace=True)
+    return df
+
+def dataFormat(data): # data must be in built in types
+    arr = {}
+    for k in data.keys():
+        if isinstance(data[k], np.integer):
+            arr[k]=int(data[k])
+        elif isinstance(data[k], np.floating):
+            arr[k]=float(data[k])
+        else :
+            arr[k]=data[k]
+    # u must add a timestamp
+    return arr
+
+def main():
+    # create a local kafka producer
+    producer = KafkaProducer(value_serializer=lambda m: js.dumps(m).encode('ascii'))
+    run_time = dt.datetime.today()
+    # get vecicles
+    df = getVehicles()
+    # iterate over vehicles
+    for i in range(0 , int(df['t'].max()*1/100) , 5):# start is 0 , end is max time , move every 5 seconds
+        currentTime = run_time + dt.timedelta(seconds=i)
+        print(str(currentTime))
+        # 1. find what to send
+        to_send = df[(df['t'] <= i + 5) & (df['t'] > i) & (df['v']>0)]
+        # 2. each car is sent in a seperate message
+        for j in range(len(to_send)) :
+            try : # AttributeError when no car
+                data = dataFormat(to_send.iloc[j])
+                data['time'] = run_time.strftime('%Y-%m-%d %H:%M:%S')
+                future = producer.send('vehicle_positions' , data)
+            except AttributeError :
+                print("No Cars")
+
+if __name__=='__main__':
+    main()
