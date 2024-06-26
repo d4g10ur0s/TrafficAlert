d28a77afcad79739da2347882feb715f6358609c
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-06-26 17:55:58 +0300

    Add files via upload

--- middleware/sparkjob.py
+++ middleware/sparkjob.py
@@ -1,19 +1,22 @@
+'''
+Run with Kafka only :
+~/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 ~/BigData/middleware/spark_job.py localhost:9092 subscribe vehicle_positions
+Run with Kafka and MongoDB :
+~/spark/bin/spark-submit --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1,org.mongodb.spark:mongo-spark-connector_2.12:10.3.0,org.mongodb:mongodb-driver-sync:5.1.0 ~/BigData/middleware/spark_job.py localhost:9092 subscribe vehicle_positions
+'''
+from pyspark import SparkContext,SparkConf
+# configure jars
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col ,count, avg, countDistinct, from_json
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType, TimestampType
+from pyspark.sql import SparkSession
 
 import sys
-
-# Create SparkSession
-spark = SparkSession.builder \
-    .appName("Vehicle Data Processing") \
-    .getOrCreate()
-
 # Setting parameters for the Spark session to read from Kafka
 bootstrapServers = sys.argv[1]
 subscribeType = sys.argv[2]
 topics = sys.argv[3]
-
 # desired schema
 schema = StructType([
     StructField("time", StringType(), True),
@@ -34,7 +37,6 @@ json_schema = StructType([
   StructField("s", DoubleType(), True),
   StructField("v", DoubleType(), True),
 ])
-
 # Read streaming data from Kafka
 df = spark \
     .readStream \
@@ -46,14 +48,13 @@ df = spark \
 raw_data = df.select(from_json(col("value").cast("string"),json_schema).alias("value"))\
        .select(*[f"value.{n}" for n in json_schema.names])
 # process data to get grouped
-grouped_data = raw_data.groupBy(col("time"), col("link"))\
-                       .agg(avg("v").alias("vspeed"), count(col("name")).alias("vcount"))
-# Replace with your desired sink configuration
+grouped_data = raw_data.withWatermark("time" , "5 seconds").groupBy(col("time"), col("link"))\
+                          .agg(avg("v").alias("vspeed"), count(col("name")).alias("vcount"))
+''' print data '''
 query = grouped_data.writeStream \
           .outputMode("complete") \
           .format("console") \
           .start()
-# Start processing streaming data
 query.awaitTermination()
 # Stop SparkSession
 spark.stop()
