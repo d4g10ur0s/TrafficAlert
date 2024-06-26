3100ac0279ae1eca26e00b2611a1c8d5ea67a42b
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2024-06-26 17:56:34 +0300

    Add files via upload

--- kafka_start.sh
+++ kafka_start.sh
@@ -9,7 +9,7 @@ SPARK_HOME=~/spark
 # Execute spark-submit command
 $SPARK_HOME/bin/spark-submit \
   --packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.0.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.1 \
-  ~/BigData/middleware/spark_job.py \
+  ~/BigData/middleware/sparkjob.py \
   localhost:9092 subscribe vehicle_positions
 
 # Check for errors in the spark-submit command
